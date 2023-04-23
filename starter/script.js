1098f36cf1e0759a2bf4144ed5908fedc24635a0
Faduma <fadumascode@gmail.com>
2023-04-24 05:15:26 +1000

    Update - refactored message

--- starter/script.js
+++ starter/script.js
@@ -6,6 +6,10 @@ console.log(secretNumber)
 let score = 20;
 let highScore = 0;
 
+const displayMessage = function(message){
+    document.querySelector('.message').textContent = message;
+}
+
 
 document.querySelector('.check').addEventListener('click', function (){
     const guess = Number((document.querySelector('.guess').value))
@@ -13,21 +17,20 @@ document.querySelector('.check').addEventListener('click', function (){
 
     // when there is no input
     if (!guess){
-        document.querySelector('.message').textContent = '⛔️ No number, please enter a guess!';
+        displayMessage('⛔️ No number, please enter a guess!')
     // when guess is correct
     } else if (guess === secretNumber){
-        document.querySelector('.message').textContent = '🎉 Correct Number';
+        displayMessage('🎉 Correct Number')
         document.querySelector('body').style.backgroundColor = '#60b347';
         document.querySelector('.number').style.width = '30rem';
         document.querySelector('.number').textContent = secretNumber;
-
         if (score >= highScore) {
             highScore = score;
             document.querySelector('.highscore').textContent = highScore;
         }        
     // when guess is incorrect
     } else if (guess != secretNumber){
-        document.querySelector('.message').textContent = guess > secretNumber ? '📈 Too High' : '📉 Too Low';;
+        displayMessage(guess > secretNumber ? '📈 Too High' : '📉 Too Low');
         score--;
         document.querySelector('.score').textContent = score; }
 });
