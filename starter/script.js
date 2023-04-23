1184a91c91c5f0fa043bdd7751cc2f1e08c0f0f1
Faduma <fadumascode@gmail.com>
2023-04-23 18:33:01 +1000

    Commit - check guess correct/too high/ too low -  add score for each guess

--- starter/script.js
+++ starter/script.js
@@ -1,4 +1,25 @@
 'use strict';
 
-console.log(document.querySelector('.message').textContent)
-document.querySelector('.message').textContent = 'Correct Number!'
+
+const secretNumber = Math.trunc(Math.random() * 20) + 1;
+console.log(secretNumber)
+let score = 20;
+document.querySelector('.number').textContent = secretNumber;
+
+document.querySelector('.check').addEventListener('click', function (){
+    const guess = Number((document.querySelector('.guess').value))
+    console.log((guess));
+
+    if (!guess){
+        document.querySelector('.message').textContent = '⛔️ No number, please enter a guess!';
+    } else if (guess === secretNumber){
+        document.querySelector('.message').textContent = '🎉 Correct Number';
+    } else if (guess > secretNumber){
+        document.querySelector('.message').textContent = '📈 Too High';
+        score--;
+        document.querySelector('.score').textContent = score;
+    } else if (guess < secretNumber){
+        document.querySelector('.message').textContent = '📉 Too Low';
+        score--;
+    }
+});
