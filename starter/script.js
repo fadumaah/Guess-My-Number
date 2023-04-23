685459f074d11fab18a1d5ce823588ff91d61780
Faduma <fadumascode@gmail.com>
2023-04-23 18:45:58 +1000

    Fix - Updated you lose score to show when score is 0 not 1

--- starter/script.js
+++ starter/script.js
@@ -15,19 +15,21 @@ document.querySelector('.check').addEventListener('click', function (){
     } else if (guess === secretNumber){
         document.querySelector('.message').textContent = '🎉 Correct Number';
     } else if (guess > secretNumber){
-        if (score > 0) {
+        if (score > 1) {
         document.querySelector('.message').textContent = '📈 Too High';
         score--;
         document.querySelector('.score').textContent = score;
         } else {
             document.querySelector('.message').textContent = '💥 You lose';
+            document.querySelector('.score').textContent = 0;
         }
     } else if (guess < secretNumber){
-        if (score > 0) {
+        if (score > 1) {
             document.querySelector('.message').textContent = '📉 Too Low';
             score--;
         } else {
             document.querySelector('.message').textContent = '💥 You lose';
+            document.querySelector('.score').textContent = 0;
         }
     }
 });
